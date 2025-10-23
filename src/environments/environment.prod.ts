ccd6da16050fd84a0a224b00f2c7ab3ecef9216f
Alfredo Tornero <e_aacarlos@stefanini.com>
2025-10-23 18:37:00 -0500

    environment prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+
+  // Backend Utils
+  API_URL_UTILS: 'https://nsf-backend-ga.azurewebsites.net/api/',
+
+  // AD
+  API_URL_AD: 'https://nsf-frontend-ga.azurewebsites.net/',
+
+};
