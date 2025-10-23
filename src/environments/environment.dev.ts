72692d2f3bc7b698da682baf5f656c3c123daf21
Alfredo Tornero <e_aacarlos@stefanini.com>
2025-10-23 18:40:27 -0500

    actualizar environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,4 +7,25 @@ export const environment = {
   // AD
   API_URL_AD: 'https://nsf-frontend-ga.azurewebsites.net/',
 
+   // Backend Client
+  API_URL_CLIENT: '',
+
+  // Backend Credit Line
+  API_URL_CREDIT_LINE: '',
+
+  // Backend Billing
+  API_URL_BILLING: '',
+
+  // Backend Liquidation
+  API_URL_LIQUIDATION: '',
+
+  // Backend Client SAP
+  API_URL_CLIENT_SAP: '',
+
+  // AD BILLING
+  API_URL_AD_BILLING: '',
+
+  // Backend CHARLIE
+  API_URL_CHARLIE: '',
+
 };
