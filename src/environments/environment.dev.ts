2a8525dbb224c83e6a19d86b173b538670be4487
Alfredo Tornero <e_aacarlos@stefanini.com>
2025-10-23 10:22:16 -0500

    actualizar env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,31 +1,10 @@
 export const environment = {
   production: false,
 
-  // Backend Client
-  API_URL_CLIENT: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/client/dev/api/',
-
   // Backend Utils
-  API_URL_UTILS: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/util/dev/api/',
-
-  // Backend Credit Line
-  API_URL_CREDIT_LINE: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/creditline/dev/api/',
-
-  // Backend Billing
-  API_URL_BILLING: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/billing/dev/api/',
-
-  // Backend Liquidation
-  API_URL_LIQUIDATION: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/liquidation/dev/api/',
-
-  // Backend Client SAP
-  API_URL_CLIENT_SAP: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/integracionsap/dev/api/',
+  API_URL_UTILS: 'https://nsf-backend-ga.azurewebsites.net/api/',
 
   // AD
-  API_URL_AD: 'https://nsf-frontend-dev.azurewebsites.net/',
-
-  // AD BILLING
-  API_URL_AD_BILLING: 'https://nsf-frontend-billing-dev.azurewebsites.net/',
-
-  // Backend CHARLIE
-  API_URL_CHARLIE: 'https://scharff-nsf-dev-apim.azure-api.net/facturacion/integracioncharlie/dev/api/',
+  API_URL_AD: 'https://nsf-frontend-ga.azurewebsites.net/',
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,31 +1,9 @@
 export const environment = {
   production: false,
 
-  // Backend Client
-  API_URL_CLIENT: 'https://nsf-backend-client-dev.azurewebsites.net/api/',
-
   // Backend Utils
-  API_URL_UTILS: 'https://nsf-backend-util.azurewebsites.net/api/',
-
-  // Backend Credit Line
-  API_URL_CREDIT_LINE: 'https://nsf-backend-credit-line.azurewebsites.net/api/',
-  // API_URL_CREDIT_LINE: 'https://localhost:7112/api/',
-
-  // Backend Billing
-  API_URL_BILLING: 'https://nsf-backend-billing.azurewebsites.net/api/',
-
-  // Backend Liquidation
-  API_URL_LIQUIDATION: 'https://nsf-backend-billing-order.azurewebsites.net/api/',
-
-  // Backend Client SAP
-  API_URL_CLIENT_SAP: 'https://nsf-backend-client-integration-dev.azurewebsites.net/api/',
+  API_URL_UTILS: 'https://nsf-backend-ga.azurewebsites.net/api/',
 
   // AD
-  API_URL_AD: 'https://nsf-frontend-dev.azurewebsites.net/',
-
-  // AD BILLING
-  API_URL_AD_BILLING: 'https://nsf-frontend-billing-dev.azurewebsites.net/',
-
-  // Backend CHARLIE
-  API_URL_CHARLIE: 'https://nsf-backend-integration-charlie.azurewebsites.net/api/',
+  API_URL_AD: 'https://nsf-frontend-ga.azurewebsites.net/',
 };
